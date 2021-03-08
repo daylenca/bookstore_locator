8ef15876ba450b74e82a8145b1a8e08362e415f3
Cody Didyk <codydidyk@gmail.com>
2021-03-07 21:54:24 -0800

    Added main and login html pages, added firebase auth to login page.

---
+++ firebase_api_bookstore.js
@@ -0,0 +1,14 @@
+  // Your web app's Firebase configuration
+  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+  var firebaseConfig = {
+    apiKey: "AIzaSyAmly4dsny5PxlNnvAuHjwXM5iVE6_psCA",
+    authDomain: "bookstore-locator-app.firebaseapp.com",
+    projectId: "bookstore-locator-app",
+    storageBucket: "bookstore-locator-app.appspot.com",
+    messagingSenderId: "782088487532",
+    appId: "1:782088487532:web:2f318e52215a2ee7a7ce49",
+    measurementId: "G-3RY5SH80LL"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+  firebase.analytics();
