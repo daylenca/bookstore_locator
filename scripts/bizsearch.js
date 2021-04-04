458a2c038849297c439ef8deb17e28f9020493d6
Cody Didyk <codydidyk@gmail.com>
2021-04-03 17:31:14 -0700

    Added minor script changes in a few places. Having errors still after review submit. Will investigate furthur later.

--- scripts/bizsearch.js
+++ scripts/bizsearch.js
@@ -1,14 +1,19 @@
+const parsedUrl = new URL(window.location.href);
+console.log(parsedUrl.searchParams.get('id')); // '123'
+
+// extract id from url, assign to variable
+var id = parsedUrl.searchParams.get('id');
+console.log(id);
+
 function getDetails() {
-  // https://some.site/?id=123
   const parsedUrl = new URL(window.location.href);
-  console.log(parsedUrl.searchParams.get("id")); // "123"
+  console.log(parsedUrl.searchParams.get('id')); // '123'
 
   // extract id from url, assign to variable
-  var id = parsedUrl.searchParams.get("id");
+  var id = parsedUrl.searchParams.get('id');
   console.log(id);
-
   // use this ID to read from firestore
-  db.collection("businesses")
+  db.collection('businesses')
   .doc(id)   //webcam ID that we extracted
   .get()
   .then(function(doc){   // display details!
@@ -19,23 +24,38 @@ function getDetails() {
       var pCode = doc.data().postalCode; 
       var phone = doc.data().phoneNumber; 
       var website = doc.data().website; 
-      $("#details-go-here").append("<h1> " + name + "</h1>");
-      $("#details-go-here").append("<h1> " + address + "</h1>");
-      $("#details-go-here").append("<h1> " + city + "</h1>");
-      $("#details-go-here").append("<h1>" + province + "</h1>");
-      $("#details-go-here").append("<h1>" + pCode + "</h1>");
-      $("#details-go-here").append("<h1>" + phone + "</h1>");
-      $("#details-go-here").append("<h1>" + website + "</h1>");
+      $('#details-go-here').append('<h1> ' + name + '</h1>');
+      $('#details-go-here').append('<h1> ' + address + '</h1>');
+      $('#details-go-here').append('<h1> ' + city + '</h1>');
+      $('#details-go-here').append('<h1>' + province + '</h1>');
+      $('#details-go-here').append('<h1>' + pCode + '</h1>');
+      $('#details-go-here').append('<h1>' + phone + '</h1>');
+      $('#details-go-here').append('<h1>' + website + '</h1>');
   })
 }
 getDetails();
 
 function openForm() {
-  document.getElementById("reviewForm").style.display = "block";
+  document.getElementById('reviewForm').style.display = 'block';
 }
 
 function closeForm() {
-  document.getElementById("reviewForm").style.display = "none";
+  document.getElementById('reviewForm').style.display = 'none';
 }
 
+const postRev = document.querySelector('#reviewForm');
+postRev.addEventListener('submit', (e) => {
+    e.preventDefault;
+    addReview();
+});
 
+ 
+function addReview() {
+  firebase.collection("reviews").add({
+    bizID: id,
+    affordability: review.affordability.value,
+    customerService: review.customerServ.value,
+    knowledge: review.knowledgeable.value,
+    productSel: review.productSelection.value,
+  })
+};
--- scripts/businesses.js
+++ scripts/businesses.js
@@ -1,21 +1,20 @@
 function displayBusinesses(){
-  db.collection("businesses")
-  .orderBy('name')
-  .get()
-  .then(function(snap){                               
+    db.collection("businesses")
+    .orderBy('name')
+    .get()
+    .then(function(snap){                               
       snap.forEach(function(doc){  
           var id = doc.id;                   
           var name = doc.data().name;             
           $("#businesses-go-here").append("<p id='" + id + "'> " + name + "</p>");
           addBusinessListener(id);
-
       })
   })
 }
 displayBusinesses();
 
 function addBusinessListener(id) {
-  document.getElementById(id)
+    document.getElementById(id)
       .addEventListener("click", function() {
           console.log(id + "was clicked!");
           //window.location.href="details.html";
