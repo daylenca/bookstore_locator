2ce0d633707125048f634b16ad38120ca5c2d6b4
Daylen Sawchuk <hi@daylen.ca>
2021-04-13 10:00:57 -0700

    Integrated map with search

--- scripts/map.js
+++ scripts/map.js
@@ -7,6 +7,25 @@ function initMap() {
     mapTypeControl: false,
     streetViewControl: false
   });
+  const geocoder = new google.maps.Geocoder();
+  document.getElementById("submit").addEventListener("click", () => {
+    geocodeAddress(geocoder, map);
+  });
+  new google.maps.Marker({
+    position: { lat: 49.3382351, lng: -123.1053874 },
+    map,
+    title: "32 Books & Gallery",
+  });
+  new google.maps.Marker({
+    position: { lat: 49.1702239, lng: -122.5803431 },
+    map,
+    title: "Wendel's Bookstore & Cafe",
+  });
+  new google.maps.Marker({
+    position: { lat: 49.2651678, lng: -122.7777308 },
+    map,
+    title: "Western Sky Books",
+  });
   new google.maps.Marker({
     position: { lat: 49.2635013, lng: -123.1029005 },
     map,
@@ -16,5 +35,20 @@ function initMap() {
     position: { lat: 49.2637373, lng: -123.1600937 },
     map,
     title: "Tanglewood Books",
+  });
+}
+
+function geocodeAddress(geocoder, resultsMap) {
+  const address = document.getElementById("address").value;
+  geocoder.geocode({ address: address }, (results, status) => {
+    if (status === "OK") {
+      resultsMap.setCenter(results[0].geometry.location);
+      new google.maps.Marker({
+        map: resultsMap,
+        position: results[0].geometry.location,
+      });
+    } else {
+      alert("Geocode was not successful for the following reason: " + status);
+    }
   });
 }
