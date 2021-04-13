b67c48c40653cebd015415df5dbed4221ebcf675
Daylen Sawchuk <hi@daylen.ca>
2021-04-13 02:39:38 -0700

    Added place markers to map

--- scripts/map.js
+++ scripts/map.js
@@ -1,11 +1,20 @@
 let map;
 
 function initMap() {
-  map = new google.maps.Map(document.getElementById("map"), {
+  const map = new google.maps.Map(document.getElementById("map"), {
     center: { lat: 49.25254717117441, lng: -123.11153465927006 },
     zoom: 12,
     mapTypeControl: false,
     streetViewControl: false
   });
+  new google.maps.Marker({
+    position: { lat: 49.2635013, lng: -123.1029005 },
+    map,
+    title: "Pulpfiction Books",
+  });
+  new google.maps.Marker({
+    position: { lat: 49.2637373, lng: -123.1600937 },
+    map,
+    title: "Tanglewood Books",
+  });
 }
-
