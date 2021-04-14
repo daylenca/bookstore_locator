e4da7d12d741f6f8d1fed900a4dcb1f0180279ee
Daylen Sawchuk <hi@daylen.ca>
2021-04-14 01:32:30 -0700

    Added infomarkers

--- scripts/map.js
+++ scripts/map.js
@@ -15,26 +15,49 @@ function initMap() {
     position: { lat: 49.3382351, lng: -123.1053874 },
     map,
     title: "32 Books & Gallery",
+    contentString: '<p><a href="bizsearch.html?id=4XRY7xjxehCSjIGntwap">32 Books & Gallery</a></p>',
   });
   new google.maps.Marker({
     position: { lat: 49.1702239, lng: -122.5803431 },
     map,
     title: "Wendel's Bookstore & Cafe",
+    contentString: '<p><a href="bizsearch.html?id=4XRY7xjxehCSjIGntwap">32 Books & Gallery</a></p>',
   });
   new google.maps.Marker({
     position: { lat: 49.2651678, lng: -122.7777308 },
     map,
     title: "Western Sky Books",
+    contentString: '<p><a href="bizsearch.html?id=4XRY7xjxehCSjIGntwap">32 Books & Gallery</a></p>',
   });
   new google.maps.Marker({
     position: { lat: 49.2635013, lng: -123.1029005 },
     map,
     title: "Pulpfiction Books",
+    contentString: '<p><a href="bizsearch.html?id=4XRY7xjxehCSjIGntwap">32 Books & Gallery</a></p>',
   });
   new google.maps.Marker({
     position: { lat: 49.2637373, lng: -123.1600937 },
     map,
     title: "Tanglewood Books",
+    contentString: '<p><a href="bizsearch.html?id=4XRY7xjxehCSjIGntwap">32 Books & Gallery</a></p>',
+  });
+  new google.maps.Marker({
+    position: { lat: 49.278652, lng: -123.098496 },
+    map,
+    title: "Massy Books",
+    contentString: '<p><a href="bizsearch.html?id=4XRY7xjxehCSjIGntwap">32 Books & Gallery</a></p>',
+  });
+  new google.maps.Marker({
+    position: { lat: 49.2675797, lng: -123.0933325 },
+    map,
+    title: "READ Books",
+    contentString: '<p><a href="bizsearch.html?id=4XRY7xjxehCSjIGntwap">32 Books & Gallery</a></p>',
+  });
+  const infowindow = new google.maps.InfoWindow({
+    content: contentString,
+  });
+  marker.addListener("click", () => {
+    infowindow.open(map, marker);
   });
 }
 
